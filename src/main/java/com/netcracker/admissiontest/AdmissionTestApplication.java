f2b0d80f1ca8e84ca29fb0031a3284794b1de95e
Andrey <lex_tor@mail.ru>
2018-03-29 23:42:59 +0300

    DeployOnTomcat

--- src/main/java/com/netcracker/admissiontest/AdmissionTestApplication.java
+++ src/main/java/com/netcracker/admissiontest/AdmissionTestApplication.java
@@ -2,9 +2,16 @@ package com.netcracker.admissiontest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AdmissionTestApplication {
+public class AdmissionTestApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AdmissionTestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdmissionTestApplication.class, args);
--- src/main/java/com/netcracker/admissiontest/user/repository/UserRepository.java
+++ src/main/java/com/netcracker/admissiontest/user/repository/UserRepository.java
@@ -3,4 +3,4 @@ package com.netcracker.admissiontest.user.repository;
 import com.netcracker.admissiontest.user.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository <User, Long>{ }
+public interface UserRepository extends CrudRepository <User, Long>{}
